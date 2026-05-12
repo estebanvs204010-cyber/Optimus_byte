0fb85dde7450e156014aec127d83d6e8a63f7449
Esteban <estebanvs2040101@gmail.com>
2026-05-12 14:11:04 -0500

    Agregar archivos de proyecto.

---
+++ Optimus_byte/Optimus_byte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Optimus_byte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
