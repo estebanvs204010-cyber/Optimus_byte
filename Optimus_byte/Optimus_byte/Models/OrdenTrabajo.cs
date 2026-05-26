2e545ca277a0e6feecc5fda95488e40b4cb35061
Esteban <estebanvs2040101@gmail.com>
2026-05-26 14:37:50 -0500

    cmabios

---
+++ Optimus_byte/Optimus_byte/Models/OrdenTrabajo.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Optimus_byte.Models
+{
+    [Table("OrdenesTrabajo")]
+    public class OrdenTrabajo
+    {
+        [Key]
+        [Column("id_orden")]
+        public int IdOrden { get; set; }
+
+        [Column("id_vehiculo")]
+        public int IdVehiculo { get; set; }
+
+        [Column("id_mecanico")]
+        public int? IdMecanico { get; set; }
+
+        [Column("id_administrador")]
+        public int IdAdministrador { get; set; }
+
+        [Column("estado")]
+        public string Estado { get; set; } = string.Empty;
+
+        [Column("tipo_servicio")]
+        public string TipoServicio { get; set; } = string.Empty;
+
+        [Column("descripcion_problema")]
+        public string DescripcionProblema { get; set; } = string.Empty;
+
+        [Column("diagnostico")]
+        public string? Diagnostico { get; set; }
+
+        [Column("observaciones")]
+        public string? Observaciones { get; set; }
+
+        [Column("km_ingreso")]
+        public int KmIngreso { get; set; }
+
+        [Column("fecha_apertura")]
+        public DateTime FechaApertura { get; set; }
+
+        [Column("fecha_cierre")]
+        public DateTime? FechaCierre { get; set; }
+
+        [ForeignKey("IdVehiculo")]
+        public Vehiculo? Vehiculo { get; set; }
+
+        [ForeignKey("IdMecanico")]
+        public Usuario? Mecanico { get; set; }
+
+        [ForeignKey("IdAdministrador")]
+        public Usuario? Administrador { get; set; }
+    }
+}
+
---
+++ Optimus_byte/Optimus_byte/Models/Vehiculo.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Optimus_byte.Models;
+
+namespace Optimus_byte.Models
+{
+    [Table("Vehiculos")]
+    public class Vehiculo
+    {
+        [Key]
+        [Column("id_vehiculo")]
+        public int IdVehiculo { get; set; }
+
+        [Column("id_cliente")]
+        public int IdCliente { get; set; }
+
+        [Required]
+        [Column("placa")]
+        public string Placa { get; set; } = string.Empty;
+
+        [Required]
+        [Column("marca")]
+        public string Marca { get; set; } = string.Empty;
+
+        [Required]
+        [Column("modelo")]
+        public string Modelo { get; set; } = string.Empty;
+
+        [Required]
+        [Column("anio")]
+        public int Anio { get; set; }
+
+        [Column("color")]
+        public string? Color { get; set; }
+
+        [Column("vin")]
+        public string? Vin { get; set; }
+
+        [Column("km_actuales")]
+        public int KmActuales { get; set; }
+
+        [Column("activo")]
+        public bool Activo { get; set; } = true;
+
+        [Column("fecha_registro")]
+        public DateTime FechaRegistro { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente? Cliente { get; set; }
+    }
+}
